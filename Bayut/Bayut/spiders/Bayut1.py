a426e707254d38db0e73b8d9e98fc1cc3ea2cef3
anjali <anjali99vinod@gmail.com>
2025-01-14 14:39:44 +0530

    User defined function start_requests created in order to store the html files of each property ids in a variable called url

--- Bayut/Bayut/spiders/Bayut1.py
+++ Bayut/Bayut/spiders/Bayut1.py
@@ -7,9 +7,10 @@ class Bayut1Spider(scrapy.Spider):
     start_urls = ["https://www.bayut.com/property/details-10398969.html"]
 
     def start_requests(self):
-        for property_id in range(9941202,9942202):
+        for property_id in range(9941202, 9942202):
             url = f"https://www.bayut.com/property/details-{property_id}.html"
             yield scrapy.Request(url=url, callback=self.parse)
+
 
     
         
